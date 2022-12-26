4d731de55ed8396fc4ebefb5261400c4c7fd761b
chenjunru <963540300@qq.com>
2022-12-26 10:41:40 +0800

    12commit

--- packages/link/src/link.vue
+++ packages/link/src/link.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: chenjunru
  * @Date: 2022-12-25 14:45:35
- * @LastEditTime: 2022-12-26 10:20:57
+ * @LastEditTime: 2022-12-26 10:38:25
  * @LastEditors: chenjunru
  * @Description: Do not Edit
  * @FilePath: \cjr-components\packages\link\src\link.vue
@@ -37,7 +37,7 @@
 <style scoped>
 	.disabled {
 		cursor: not-allowed;
-		font-size: 16px;
+		font-size: 12px;
 	}
 	.no-underline {
 		text-decoration: none;
