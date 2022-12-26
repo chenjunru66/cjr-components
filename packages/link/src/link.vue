846ac5e230ed3575b793e2696b999e77a44ae223
chenjunru <963540300@qq.com>
2022-12-26 09:45:18 +0800

    ten commit

--- packages/link/src/link.vue
+++ packages/link/src/link.vue
@@ -1,42 +1,45 @@
 <!--
  * @Author: chenjunru
  * @Date: 2022-12-25 14:45:35
- * @LastEditTime: 2022-12-25 19:53:57
+ * @LastEditTime: 2022-12-26 09:44:52
  * @LastEditors: chenjunru
  * @Description: Do not Edit
- * @FilePath: \my-components\packages\link\src\link.vue
+ * @FilePath: \cjr-components\packages\link\src\link.vue
 -->
 <template>
-  <a :href="disabled ? null : href" :class="[disabled && 'disabled', !underline && 'no-underline']">
-    <slot></slot>
-  </a>
+	<a
+		:href="disabled ? null : href"
+		:class="[disabled && 'disabled', !underline && 'no-underline']"
+	>
+		<slot></slot>
+	</a>
 </template>
 
 <script>
-export default {
-  name: 'CjrLink',
-  props: {
-    href: {
-      type: String
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    underline: {
-      type: Boolean,
-      default: false
-    }
-  }
-}
+	export default {
+		name: "CjrLink",
+		props: {
+			href: {
+				type: String,
+			},
+			disabled: {
+				type: Boolean,
+				default: false,
+			},
+			underline: {
+				type: Boolean,
+				default: false,
+			},
+		},
+	}
 </script>
 
 <style scoped>
-.disabled {
-  cursor: not-allowed;
-  font-size: 18px;
-}
-.no-underline {
-  text-decoration: none;
-}
+	.disabled {
+		cursor: not-allowed;
+		font-size: 16px;
+	}
+	.no-underline {
+		text-decoration: none;
+	}
 </style>
