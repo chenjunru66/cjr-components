f02aa1c5d691cb28a9ff33be4cc306394957ddf8
chenjunru <963540300@qq.com>
2022-12-30 17:19:50 +0800

    21commit

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,15 +1,22 @@
+<!--
+ * @Author: chenjunru
+ * @Date: 2022-12-27 09:13:35
+ * @LastEditTime: 2022-12-30 15:54:15
+ * @LastEditors: chenjunru
+ * @Description: Do not Edit
+ * @FilePath: \cjr-components\packages\button\src\button.vue
+-->
 <template>
-  <div>
-  </div>
+	<button>
+		<slot></slot>
+	</button>
 </template>
 
 <script>
-export default {
-  name: 'CjrButton',
-  props: {
-  }
-}
+	export default {
+		name: "CjrButton",
+		props: {},
+	}
 </script>
 
-<style>
-</style>
+<style></style>
--- packages/button/stories/button.stories.js
+++ packages/button/stories/button.stories.js
@@ -1,15 +1,23 @@
-import CjrButton from '../src/button.vue'
+/*
+ * @Author: chenjunru
+ * @Date: 2022-12-27 09:13:35
+ * @LastEditTime: 2022-12-30 15:53:36
+ * @LastEditors: chenjunru
+ * @Description: Do not Edit
+ * @FilePath: \cjr-components\packages\button\stories\button.stories.js
+ */
+import CjrButton from "../src/button.vue"
 
 export default {
-  title: 'CjrButton',
-  component: CjrButton
+	title: "CjrButton",
+	component: CjrButton,
 }
 
-export const Button = _ => ({
-  components: { CjrButton },
-  template: `
+export const Button = (_) => ({
+	components: { CjrButton },
+	template: `
     <div>
-      <cjr-button></cjr-button>
+      <cjr-button>按钮</cjr-button>
     </div>
-  `
+  `,
 })
